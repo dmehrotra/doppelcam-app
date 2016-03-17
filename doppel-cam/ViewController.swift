9e3f90ad310a04b9738132f40a5170cfc0a7814f
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-03-17 16:15:47 -0400

    initial commit

---
+++ doppel-cam/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  doppel-cam
+//
+//  Created by Dhruv on 3/17/16.
+//  Copyright © 2016 Dhruv. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var imageView: UIImageView!
+    var newMedia: Bool?
+    
+    @IBAction func useCamera(sender: AnyObject) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera){
+            
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .Camera
+            
+            self.presentViewController(picker, animated: true,
+                completion: nil)
+        
+        
+            newMedia = true;
+            
+        }
+        
+        
+    }
+    @IBAction func useCameraRoll(sender: AnyObject) {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
+        newMedia = false;
+    
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func  imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        let mediaType = info[UIImagePickerControllerMediaType] as! String
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+            
+        if (newMedia == true) {
+            UIImageWriteToSavedPhotosAlbum(image, self, "image:didFinishSavingWithError:contextInfo:", nil)
+        }
+        
+    }
+    func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafePointer<Void>) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                message: "Failed to save image",
+                preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                style: .Cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.presentViewController(alert, animated: true,
+                completion: nil)
+        }
+    }
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
