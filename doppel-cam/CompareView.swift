94fc4400355eb3e3d25b1ec1b37107e1d4f8067f
Dhruv Mehrotra <dhruv.mehrotra3@gmail.com>
2016-04-30 14:42:55 -0400

    initial share

--- doppel-cam/CompareView.swift
+++ doppel-cam/CompareView.swift
@@ -8,22 +8,46 @@
 
 import Foundation
 import UIKit
-
+import Social
 class Compare:UIViewController{
     
     @IBOutlet weak var doppelPic: UIImageView!
     
     @IBOutlet weak var chosenPic: UIImageView!
-   
+    @IBAction func shareToFacebook(){
+        let shareToFacebook: SLComposeViewController =
+        SLComposeViewController(forServiceType:
+        SLServiceTypeFacebook)
+        shareToFacebook.setInitialText(<#T##text: String!##String!#>)
+        
+        self.presentViewController(shareToFacebook, animated: true, completion: nil)
+    }
+    @IBAction func shareToTwitter(){
+        let shareToTwitter: SLComposeViewController =
+        SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        
+        self.presentViewController(shareToTwitter, animated: true, completion: nil)
+        
+    }
     var doppelImage = UIImage()
     var chosenImage = UIImage()
 
     override func viewDidLoad() {
         doppelPic.image = doppelImage
         chosenPic.image = chosenImage
+        
     }
     
+    func takeScreenshot(view: UIView) -> UIImageView {
+        UIGraphicsBeginImageContext(view.frame.size)
+        view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        
 
+        return UIImageView(image: image)
+    }
     
     
 
